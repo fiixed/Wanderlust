177f54f00f7f465694b353f73e31094b45ff9809
fiixed <fiixed@gmail.com>
2020-12-08 20:21:57 -0500

    home route

--- index.js
+++ index.js
@@ -13,14 +13,12 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
+app.get('/', (req, res) => {
+  res.render('home');
+});
 
 
 
-
-
-
-
-
-app.listen(3000, function() {
+app.listen(3000, () => {
   console.log("Server started on port 3000");
 });
